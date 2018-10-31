a49ab0b8e11815174add13938bca7a854a277624
Ahmad M. Osman <ahmad.osman@luther.edu>
2018-10-31 13:10:02 -0500

    Finished Trie

--- Programming Exercises/Trie/osmanTrie.py
+++ Programming Exercises/Trie/osmanTrie.py
@@ -3,38 +3,42 @@
 # Dr. Lee, please notice that if multiple words were seperated by nothing but punctuation, like "Happiness.--That", my string manipulation to remove punctuation will transfer it into "happinessthat". I did not want to put more time into cleaning the words as leaving "happinessthat" like it that is helping to test the Bloom Filter.
 
 
-import math
 import string
 
 
 class Trie:
     def __insert(node, item):
+        # This is the recursive insert function.
         if len(item) == 0:
-            raise ValueError("Inserting empty item into Trie!")
-
-        if node == None:
+            return None
+        elif node == None:
             node = Trie.TrieNode(item[0])
-            item = item[1:]
+        if node.item == item[0]:
+            if node.follows is None:
+                if item == "$":
+                    Trie.__insert(None, "")
+                else:
+                    node.follows = Trie.__insert(Trie.TrieNode(item[1]), item[1:])
+            else:
+                Trie.__insert(node.follows, item[1:])
+        else:
+            if node.next is None:
+                node.next = Trie.__insert(Trie.TrieNode(item[0]), item)
+            else:
+                Trie.__insert(node.next, item)
 
-        original_node = node
-
-        for letter in item:
-            if node.item == letter:
-                print(letter)
-                node = node.follows
-                continue
-            elif node.item == None:
-                node.item = letter
-                node.follows = Trie.TrieNode(letter)
-                node = node.follows
-            elif node.item != letter:
-                node.next = Trie.TrieNode(letter)
-                node = node.next
+        return node
 
     def __contains(node, item):
-        """# This is the recursive membership test.
-        pass """
-        pass
+        # This is the recursive membership test.
+        if len(item) == 0:
+            return True
+        elif node == None:
+            return False
+        elif node.item == item[0]:
+            return Trie.__contains(node.follows, item[1:])
+        else:
+            return Trie.__contains(node.next, item)
 
     class TrieNode:
         def __init__(self, item, next=None, follows=None):
@@ -46,9 +50,11 @@ class Trie:
         self.start = None
 
     def insert(self, item):
+        item += "$"
         self.start = Trie.__insert(self.start, item)
 
     def __contains__(self, item):
+        item += "$"
         return Trie.__contains(self.start, item)
 
 
@@ -60,11 +66,9 @@ def main():
     trie = Trie()
     for word in words:
         word = word.strip()
-        word += '$'
         trie.insert(word)
 
-
-"""     # count = 0
+    # count = 0
     with open("declaration.txt") as openfile:
         exclude = set(string.punctuation)
         for line in openfile:
@@ -73,10 +77,10 @@ def main():
                 word = ''.join(ch for ch in word if ch not in exclude)
                 # lowering
                 word = word.lower()
-                if word not in bloom:
+                if word not in trie:
                     # count += 1
                     print(word)
-    # print(count) """
+    # print(count)
 
 
 if __name__ == "__main__":
